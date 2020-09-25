ed5d8a9db4e822dd90f6e27e2756f6d18ae33004
viniciussec <viniciussec@hotmail.com>
2020-09-25 15:35:22 -0300

    frontend configuration

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,7 +1,18 @@
+import React from "react"
+import Head from "next/head"
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 export default function Home() {
     return (
-        <div>
-            
+        <div className="container">
+            <Head>
+
+            </Head>
+            <main>
+                asdasd
+
+
+            </main>
         </div>
     )
 }
