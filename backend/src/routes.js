2fd12969cbbd2643cebfdb7dc0b1121151ecf5ad
viniciussec <viniciussec@hotmail.com>
2020-09-26 16:57:26 -0300

    exclusive user backend request

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -5,9 +5,17 @@ const routes = Router()
 
 let data
 
+
 routes.get('/', async (req, res) => {
     const apiRes = await axios.get("https://api.github.com/users")
-    //console.log(apiRes.data)
+
+    return res.json(apiRes.data)
+})
+
+
+routes.get('/:login', async (req, res) => {
+    console.log(req.params)
+    const apiRes = await axios.get(`https://api.github.com/users/${req.params.login}`)
 
     return res.json(apiRes.data)
 })
--- frontend/components/Users.jsx
+++ frontend/components/Users.jsx
@@ -1,13 +1,15 @@
 import React, { useEffect, useState } from "react"
 import '../styles/Users.css'
 import api from '../services/api'
-import { Collapse, Button, CardBody, Card } from 'reactstrap';
+import { Collapse, CardBody, Card } from 'reactstrap';
 
 export default function Users() {
     const [users, setUsers] = useState([])
     const [isOpen, setIsOpen] = useState(false);
-    const toggle = () => setIsOpen(!isOpen);
-
+    const toggle = (e) => {
+        console.log(e.target.id)
+        setIsOpen(!isOpen)
+    }
 
     useEffect(() => {
         async function loadUsers() {
@@ -25,13 +27,13 @@ export default function Users() {
                 <li>
                     <div className="user-space">
                         <div className="closed-user">
-                            <img onClick={toggle} src={user.avatar_url} alt={user.login} />
-                            <h3 onClick={toggle} className="nickName">{user.login}</h3>
+                            <img onClick={toggle} src={user.avatar_url} alt={user.login} id={user.login}/>
+                            <h3 onClick={toggle} className="nickName" id={user.login}>{user.login}</h3>
                         </div>
                         <Collapse isOpen={isOpen}>
                             <Card className="accordion">
-                                <CardBody>
-                                    User URL: {user.url}
+                                <CardBody className="accordion-text">
+                                    <label>User URL: {user.url}</label>
                                 </CardBody>
                             </Card>
                         </Collapse>
